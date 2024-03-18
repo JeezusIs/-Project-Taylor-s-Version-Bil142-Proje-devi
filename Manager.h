a7ed940b6525003b1365f2ab524bcc1d109676e6
¥eezus (Taylor's Version) <158213403+JeezusIs@users.noreply.github.com>
2024-03-18 16:36:55 +0300

    Create Manager.h
    Manager.h header dosyası. Bu header dosyasında main'de kullanacağımız fonksiyonlar tanımlanmıştır.

---
+++ Manager.h
@@ -0,0 +1,16 @@
+#define UNTITLED2_MANAGER_H
+
+
+#include <iostream>
+#include "Spaceship.h"
+#include <memory>
+
+class Spaceship;
+int  RandomNumberGenerator(int start, int end);
+std::shared_ptr<Spaceship> getSelectedShip();
+void RandomEventGenerator(std::shared_ptr<Spaceship>Ship);
+void ScoreCalculator(std::shared_ptr<Spaceship> selectedShip);
+
+
+
+#endif //UNTITLED2_MANAGER_H
